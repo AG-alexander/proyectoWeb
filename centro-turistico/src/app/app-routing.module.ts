f7fa4df05b61f4176454bad9d9af6afb579d2a9b
amoucles <alexcuatroojos@gmail.com>
2019-03-21 11:26:50 -0600

    home page and routing home ready

--- centro-turistico/src/app/app-routing.module.ts
+++ centro-turistico/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path: 'dashboard', component: DashboardComponent},
+  {path: 'dashboard', component: DashboardComponent, children: [
+    {path: 'home', component: HomeComponent},
+    {path: '', pathMatch: 'full', redirectTo: 'home'}
+
+  ]},
   {path: '**', pathMatch: 'full', redirectTo: 'dashboard'}
 ];
 
