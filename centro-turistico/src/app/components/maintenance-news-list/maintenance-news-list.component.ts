e67c58a48f279cc5371465696ab6a304bf1544b4
amoucles <alexcuatroojos@gmail.com>
2019-04-06 20:13:49 -0600

    mantenimiento noticias listar

--- centro-turistico/src/app/components/maintenance-news-list/maintenance-news-list.component.ts
+++ centro-turistico/src/app/components/maintenance-news-list/maintenance-news-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { NewsService } from '../../services/index';
+import { News } from 'src/app/interfaces/index';
 @Component({
   selector: 'app-maintenance-news-list',
   templateUrl: './maintenance-news-list.component.html',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaintenanceNewsListComponent implements OnInit {
 
-  constructor() { }
+  news_list: News[];
+  constructor(private _newsService: NewsService, private _router: Router) { }
 
+  addNew(){
+    this._router.navigate(['dashboard/mainte-news-set']);
+  }
+
+  updateNew(id: number){
+    this._router.navigate(['dashboard/mainte-news-up', id]);
+  }
+  
   ngOnInit() {
+    this.news_list = this._newsService.getNews();
   }
 
 }
--- centro-turistico/src/app/services/news.service.ts
+++ centro-turistico/src/app/services/news.service.ts
@@ -13,4 +13,23 @@ export class NewsService {
   getNews(): News[] {
     return this._dataStorage.getObjectValue(constant.NEWS) as News[];
   }
+
+  saveNew(news: News) {
+    let id: number = 0;
+    let news_list = this._dataStorage.getObjectValue(constant.NEWS) as News[];
+
+    news_list.forEach((item)=>{
+      if (item.idNews == news.idNews) {
+        id = item.idNews;
+      }
+    });
+
+    if (id > 0) {
+      news_list[id] = news;
+    } else {
+      news_list.push(news);
+    }
+
+    this._dataStorage.setObjectValue(constant.NEWS, news_list);
+  }
 }
