7145ea174bc39ec7f2220518a2ee09b3888a35f2
amoucles <alexcuatroojos@gmail.com>
2019-04-10 16:07:39 -0600

    ajuste de aceso a la barra, comentarios de usuario en su perfil

--- centro-turistico/src/app/components/user-profile/user-profile.component.ts
+++ centro-turistico/src/app/components/user-profile/user-profile.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService, DataStorageService } from 'src/app/services/index';
+import { UserService, DataStorageService, SiteService } from 'src/app/services/index';
 import { ActivatedRoute } from '@angular/router';
-import { User } from 'src/app/interfaces/index';
+import { User, followerModel, TouristicCentre, userInfo, Review } from 'src/app/interfaces/index';
 import { constant } from 'src/app/constant-data/constant';
 import { Console } from '@angular/core/src/console';
+import { Rating } from 'src/app/interfaces/rating';
 
 @Component({
   selector: 'app-user-profile',
@@ -14,19 +15,52 @@ export class UserProfileComponent implements OnInit {
 
   iduser: number;
   user: User
-
-  constructor(private _userService: UserService, 
+  follower_list: followerModel[];
+  rating_list: Rating[];
+  site_list: TouristicCentre[];
+  reviews_list: Review[];
+  userSites: TouristicCentre;
+  userInfo: userInfo[];
+  constructor(private _userService: UserService,
     private _activatedRouete: ActivatedRoute,
     private _storage: DataStorageService) { }
 
   ngOnInit() {
     this.iduser = +this._activatedRouete.snapshot.params['id'];
     this.user = this._userService.getUser();
-    console.log(this.user);
-    console.log(this._storage.getObjectValue(constant.FOLLOWERS));
-    console.log(this._storage.getObjectValue(constant.RATINGS));
-    console.log(this._storage.getObjectValue(constant.SITES));
+    this.follower_list = this._storage.getObjectValue(constant.FOLLOWERS);
+    this.rating_list = this._storage.getObjectValue(constant.RATINGS);
+    this.site_list = this._storage.getObjectValue(constant.SITES);
+    this.reviews_list = this._storage.getObjectValue(constant.REVIEWS);
+    let temoUserInfo: userInfo;
+    this.userInfo = [];
     
+    let tempFollower = this.follower_list.filter(item => item.userId == this.user.idUser);
+    let sitesFollow: TouristicCentre[] = [];
+    tempFollower.forEach((item) => {
+      sitesFollow.push(this.site_list.find(items => items.idTouristicCentre == item.siteId));
+    });
+    let ratFollow: Rating[] = [];
+
+    this.rating_list.forEach((item) =>{
+      if (item.idUser == this.iduser) {
+        ratFollow.push(item);
+      }
+    });
+
+    let tempReviews: Review[];
+    tempReviews = this.reviews_list.filter(item => item.idUser == this.user.idUser);
+   sitesFollow.forEach((item)=>{
+     let temp: userInfo = {
+       nameSite: item.name,
+       ratingSite: this.getRating(ratFollow.find(itemm => itemm.idTouristicCentre == item.idTouristicCentre)),
+       reviews_list: tempReviews.filter(itemm => itemm.idSitio == item.idTouristicCentre)
+     }
+     this.userInfo.push(temp);
+   });
+  }
+  getRating(r: Rating): number {
+    return r!=null? r.rating: 0;
   }
 
 }
--- centro-turistico/src/app/interfaces/index.ts
+++ centro-turistico/src/app/interfaces/index.ts
@@ -2,7 +2,7 @@ import { from } from 'rxjs';
 
 
 export { News } from './news';
-export { TouristicCentre, followerXSite, ratingXSite, reviews, reviewsModel, followerModel} from './touristic-centre';
+export { TouristicCentre, followerXSite, ratingXSite, reviews, reviewsModel, followerModel, userInfo} from './touristic-centre';
 export { User, EditorxSitio} from './user';
 export { photos, imageXSite, imageXUser} from './galery';
 export { permXRol, perms, rolXuser, roles } from './perms';
