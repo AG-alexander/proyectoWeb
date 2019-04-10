3a0b5590623b817b07f55b23e6aecc3a7ca1cd75
aAriasCR <alanjarias@hotmail.com>
2019-04-09 21:32:02 -0600

    cambios alert

--- centro-turistico/src/app/services/index.ts
+++ centro-turistico/src/app/services/index.ts
@@ -1,3 +1,5 @@
+import { from } from 'rxjs';
+
 export { SiteService } from '../services/site.service';
 export { DataStorageService } from '../services/data-storage.service';
 export { FollowerService } from '../services/follower.service';
@@ -6,3 +8,4 @@ export { RatingService } from '../services/rating.service';
 export { LoginService } from '../services/LoginService';
 export { NewsService } from '../services/news.service';
 export { UserService } from '../services/user.service';
+export { AlertService } from '../services/alert.service'
